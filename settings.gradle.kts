9e8dcea25d2665f2924d2c48c091c4fd18389dbf
Oscar Lakra <olakra@users.noreply.github.com>
2022-01-08 23:12:49 -0500

    Adding coin list feature as a module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,12 @@ include(
     // Features
     ":features:coin-list"
 )
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() // Warning: this repository is going to shut down soon
+    }
+}
